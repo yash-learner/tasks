5ede2afcb9b4ed57890e0acfdc3f21252b123bbd
yash-learner <yash.topaz@gmail.com>
2024-11-05 01:56:53 +0530

    Fix import typos

--- tasks/urls.py
+++ tasks/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import DefaultRouter
+from rest_framework.routers import DefaultRouter
 from taskmanager.views import TaskViewSet
 
 router = DefaultRouter()
