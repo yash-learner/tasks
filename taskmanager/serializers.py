5afbb9a9e2aab299ec50bcc3045caba31b885b53
yash-learner <yash.topaz@gmail.com>
2024-11-05 01:37:50 +0530

    Add serializer for Task

---
+++ taskmanager/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "id",
+            "title",
+            "description",
+            "completed",
+            "created_at",
+            "updated_at",
+            "user",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
