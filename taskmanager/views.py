6761f4af579a31803a4c92d9dde059fa6dd790ff
yash-learner <yash.topaz@gmail.com>
2024-11-05 01:47:04 +0530

    Add task view set

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
